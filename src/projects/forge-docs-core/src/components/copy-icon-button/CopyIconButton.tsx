010eea7a6cbf622b8a3d5108bc27e79c38448012
Kieran Nichols <kieran.nichols@tylertech.com>
2022-06-27 10:03:42 -0400

    feat: oss prep (#2)
    * feat: oss prep
    
    * chore: disabled `isolatedModules` for Auto

--- src/projects/forge-docs-core/src/components/copy-icon-button/CopyIconButton.tsx
+++ src/projects/forge-docs-core/src/components/copy-icon-button/CopyIconButton.tsx
@@ -1,13 +1,23 @@
-import { ForgeIconButton, ForgeTooltip } from '@tylertech/forge-react';
-import React, { FC, MouseEventHandler } from 'react';
+import { ForgeIcon, ForgeIconButton, ForgeTooltip } from '@tylertech/forge-react';
+import { IconRegistry } from '@tylertech/forge';
+import { tylIconContentCopy } from '@tylertech/tyler-icons/standard';
+import React, { FC, MouseEventHandler, useEffect } from 'react';
 
 export interface CopyIconButtonArgs {
   onCopy: MouseEventHandler<HTMLButtonElement>;
 }
 
-export const CopyIconButton: FC<CopyIconButtonArgs> = ({ onCopy }) => (
-  <ForgeIconButton class="forge-docs-core__copy-button">
-    <button type="button" className="tyler-icons" onClick={onCopy}>content_copy</button>
-    <ForgeTooltip position="bottom">Copy code snippet</ForgeTooltip>
-  </ForgeIconButton>
-);
+export const CopyIconButton: FC<CopyIconButtonArgs> = ({ onCopy }) => {
+  useEffect(() => {
+    IconRegistry.define(tylIconContentCopy);
+  }, []);
+
+  return (
+    <ForgeIconButton class="forge-docs-core__copy-button">
+      <button type="button" onClick={onCopy}>
+        <ForgeIcon name="content_copy" />
+      </button>
+      <ForgeTooltip position="bottom">Copy code snippet</ForgeTooltip>
+    </ForgeIconButton>
+  );
+};
